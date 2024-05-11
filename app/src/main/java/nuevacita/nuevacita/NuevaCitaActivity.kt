c2e3a66f0900d06547c046013f721dac09fb1559
Marcela Mazo <marcela.mazo@correounivalle.edu.co>
2024-05-10 19:55:15 -0500

    AutoCompleteTextView
    Se realizó un cuadro de auto completado. Se solucionaron errores.

--- app/src/main/java/nuevacita/nuevacita/NuevaCitaActivity.kt
+++ app/src/main/java/nuevacita/nuevacita/NuevaCitaActivity.kt
@@ -1,28 +1,63 @@
 package nuevacita.nuevacita
 
 import android.os.Bundle
-import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import nuevacita.nuevacita.R
+import android.widget.AutoCompleteTextView
+import android.widget.ArrayAdapter
+import com.example.dogapp.R
+import com.example.dogapp.api.RetrofitClient
+import com.example.dogapp.model.DogBreedsResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
 
 class NuevaCitaActivity : AppCompatActivity() {
+    private lateinit var autoCompleteTextViewBreed: AutoCompleteTextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_appointment)
+        setContentView(R.layout.activity_nueva_cita)
 
+        //Esto es para el toolbar
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        toolbar.setNavigationIcon(R.drawable.baseline_arrow_back_ios_pink)
-
-        // Manejar el evento de presión del botón de regreso con OnBackPressedDispatcher
-        onBackPressedDispatcher.addCallback(this) {
-            // Aquí puedes manejar el regreso
-            // Por ejemplo: Navegar a otro fragmento o simplemente llamar a finish()
-            finish()  // Esto cerrará la actividad actual
+        // Configurar el botón de regreso
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
         }
+
+        // Inicializar AutoCompleteTextView
+        autoCompleteTextViewBreed = findViewById(R.id.autoCompleteTextViewBreed)
+
+        // Obtener la lista de razas de perros utilizando Retrofit
+        obtenerListaRazasPerros()
+    }
+
+    private fun obtenerListaRazasPerros() {
+        val call: Call<DogBreedsResponse> = RetrofitClient.dogApiService.getAllBreeds()
+
+        call.enqueue(object : Callback<DogBreedsResponse> {
+            override fun onResponse(call: Call<DogBreedsResponse>, response: Response<DogBreedsResponse>) {
+                if (response.isSuccessful) {
+                    val breedsResponse = response.body()
+                    val breeds = breedsResponse?.message?.keys?.toList() ?: listOf()
+
+                    // Configurar el adaptador para AutoCompleteTextView
+                    val adapter = ArrayAdapter(this@NuevaCitaActivity, android.R.layout.simple_dropdown_item_1line, breeds)
+                    autoCompleteTextViewBreed.setAdapter(adapter)
+                }
+            }
+
+            override fun onFailure(call: Call<DogBreedsResponse>, t: Throwable) {
+                // Manejar el fallo de la solicitud
+            }
+        })
     }
 }
+
+
 
