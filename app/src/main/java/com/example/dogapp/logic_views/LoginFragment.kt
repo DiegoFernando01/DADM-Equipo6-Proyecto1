da1d262914e906c8231f958090d7311c8199e01f
DiegoFernando01 <diegofvictoria2013@gmail.com>
2024-05-12 19:26:09 -0500

    Adds base app logics and views to implement fragments

---
+++ app/src/main/java/com/example/dogapp/logic_views/LoginFragment.kt
@@ -0,0 +1,24 @@
+package com.example.dogapp.logic_views
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dogapp.R
+
+class LoginFragment : Fragment() { // Fragmento login
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView( // Operaciones sobre la vista durante su creación
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+}
