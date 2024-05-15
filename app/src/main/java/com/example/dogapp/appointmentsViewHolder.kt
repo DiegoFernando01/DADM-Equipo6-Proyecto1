7e55271568a4087e8b934275f1c4269d9f45302b
SebastianSalas <janiersalas09@gmail.com>
2024-05-14 22:46:07 -0500

    Adds base listener event to complete requirement #5

--- app/src/main/java/com/example/dogapp/appointmentsViewHolder.kt
+++ app/src/main/java/com/example/dogapp/appointmentsViewHolder.kt
@@ -1,17 +1,23 @@
 package com.example.dogapp
 
 import android.view.View
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.dogapp.databinding.FragmentHomeBinding
 import com.example.dogapp.databinding.ItemAppointmentBinding
 
 class appointmentsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     val binding = ItemAppointmentBinding.bind(view)
+    val bindingHome = FragmentHomeBinding.bind(view)
 
     fun render(appointmentsModel: appointments) {
         binding.tvItemName.text = appointmentsModel.name
         binding.tvItemDescription.text = appointmentsModel.description
         binding.tvItemOrder.text = appointmentsModel.order
+        
+        // Agregar imagen
+        itemView.setOnClickListener() {
+            // Evento al presionar un item
+        }
     }
 }
