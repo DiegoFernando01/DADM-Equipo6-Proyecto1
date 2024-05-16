1d24689341ad22888c944dae18dc508a9da2a4de
Marcela Mazo <marcela.mazo@correounivalle.edu.co>
2024-05-16 15:12:07 -0500

    api de imagenes

--- app/src/main/java/com/example/dogapp/DogBreedsResponse.kt
+++ app/src/main/java/com/example/dogapp/DogBreedsResponse.kt
@@ -4,3 +4,8 @@ data class DogBreedsResponse(
     val message: Map<String, List<String>>,
     val status: String
 )
+
+data class ImageResponse(
+    val message: String,
+    val status: String
+)
--- app/src/main/java/com/example/dogapp/webservice/DogApiService.kt
+++ app/src/main/java/com/example/dogapp/webservice/DogApiService.kt
@@ -2,10 +2,16 @@ package com.example.dogapp.webservice
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import com.example.dogapp.DogBreedsResponse
+import com.example.dogapp.ImageResponse
 import com.example.dogapp.utils.Constants.END_POINT
 
 interface DogApiService {
     @GET(END_POINT)
     fun getAllBreeds(): Call<DogBreedsResponse>
+
+    @GET("breed/{breed}/images/random")
+    fun getBreedImage(@Path("breed") breed: String): Call<ImageResponse>
 }
+
