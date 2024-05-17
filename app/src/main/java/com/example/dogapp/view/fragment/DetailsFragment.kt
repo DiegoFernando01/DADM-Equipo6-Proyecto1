a17019b316114f2c1ebdb3b5726ef7216fa4fc8a
DiegoFernando01 <diegofvictoria2013@gmail.com>
2024-05-16 23:16:30 -0500

    Adds delete appointment query to details fragment

--- app/src/main/java/com/example/dogapp/view/fragment/DetailsFragment.kt
+++ app/src/main/java/com/example/dogapp/view/fragment/DetailsFragment.kt
@@ -5,30 +5,59 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.dogapp.R
 import com.example.dogapp.databinding.FragmentDetailsBinding
+import com.example.dogapp.model.Appointment
+import com.example.dogapp.viewmodel.AppointmentViewModel
 
 class DetailsFragment : Fragment() { // Fragmento detalle citas
 
-    private var _binding: FragmentDetailsBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentDetailsBinding
+    private val appointmentViewModel: AppointmentViewModel by viewModels()
+    private lateinit var receivedAppointment: Appointment
 
     override fun onCreateView( // Operaciones sobre la vista durante su creación
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-
-        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+    ): View? {
+        binding = FragmentDetailsBinding.inflate(inflater)
+        binding.lifecycleOwner = this
         return binding.root
-
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) { // Operaciones sobre la vista después de creada
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) { // Operaciones sobre la vista después de creada
         super.onViewCreated(view, savedInstanceState)
-
+        dataAppointment()
+        controllers()
     }
 
-    override fun onDestroyView() { // Operaciones sobre la vista durante su destrucción
-        super.onDestroyView()
-        _binding = null
+    private fun controllers() { // Eventos de la vista
+        binding.buttonDelete.setOnClickListener() {
+            deleteAppointment()
+        }
+        binding.buttonEditar.setOnClickListener() {
+            val bundle = Bundle()
+            bundle.putSerializable("dataAppointment", receivedAppointment)
+            findNavController().navigate(R.id.action_DetailsFragment_to_EditFragment)
+        }
+    }
+
+    private fun deleteAppointment() { // Eliminar en BD cita
+        appointmentViewModel.deleteAppointment(receivedAppointment)
+        appointmentViewModel.getAllAppointments()
+        findNavController().popBackStack()
+        // Mensaje de registro eliminado con éxito
+    }
+
+    private fun dataAppointment() { // Editar en BD cita
+        val receivedBundle = arguments
+        receivedAppointment = receivedBundle?.getSerializable("clave") as Appointment
+        binding.tvDetailsPetName.text = "${receivedAppointment.name}"
+        // Agregar demás campos de texto
     }
 }
